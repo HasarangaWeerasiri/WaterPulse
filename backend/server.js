09bf550b23bfe4fc86159a31820df84187e7f797
Lakshan <lahirulakshan2701@gmail.com>
2026-02-23 13:28:25 +0530

    safe zone component done🫡

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,42 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
 import reportRoutes from "./routes/reportRoutes.js";
+import safeZoneRoutes from "./routes/safeZoneRoutes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 
 // Auth routes
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 // Health check
-app.get('/api/health', (req, res) => {
-  res.json({ message: 'Server is healthy' });
+app.get("/api/health", (req, res) => {
+  res.json({ message: "Server is healthy" });
 });
 
 // Report routes
 app.use("/api/reports", reportRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
- .then(() => { 
-   console.log("MongoDB connection established Successfully");
+// Safe Zone routes
+app.use("/api/safe-zones", safeZoneRoutes);
 
-   app.listen(PORT, () => {
-     console.log("Server is running on port " + PORT);
-   });
- })
- .catch((err) =>{
-   console.error("MongoDB connection error:", err);
- });
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connection established Successfully");
 
-
+    app.listen(PORT, () => {
+      console.log("Server is running on port " + PORT);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
